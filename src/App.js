1f7d0aadb5135e35f0c9bc616e84123e478274db
daniyaltayyab <daniyaltayyab91@gmail.com>
2022-10-22 16:06:24 +0500

    feature: Directory Component & catergory item component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./categories.styles.scss";
+import Directory from "./components/directory/Directory";
+
+const App = () => {
+  return (
+    <>
+      <Directory />
+    </>
+  );
+};
+
+export default App;
